ae5f5deceb12efe9c1088551ed108b7bef187a30
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-06-10 13:43:42 +0800

    Removed the hello world function.

--- zesthelpers.js
+++ zesthelpers.js
@@ -1,10 +1,6 @@
 const { notDefined } = require('./functions');
 const { ZestApi } = require('./ZestApi');
 
-const helloWorld = () => {
-	console.log('Hello World');
-};
-
 module.exports.notDefined = notDefined;
 module.exports.ZestApi = ZestApi;
-module.exports.helloWorld = helloWorld;
+
