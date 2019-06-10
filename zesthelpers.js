e3f0d1deaeffeeb7c3c1acf15f7b7cba30bdabd1
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-06-10 13:54:06 +0800

    Added comment where to export all modules.

--- zesthelpers.js
+++ zesthelpers.js
@@ -1,6 +1,7 @@
 const { notDefined } = require('./functions');
 const { ZestApi } = require('./ZestApi');
 
+// export all modules here
 module.exports.notDefined = notDefined;
 module.exports.ZestApi = ZestApi;
 
