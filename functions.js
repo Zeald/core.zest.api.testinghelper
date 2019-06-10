d69743cd1a7e48e3ec03eec2f8e0eb3ef22d3342
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-06-10 13:51:05 +0800

    Added comment documentation for notDefined function.

--- functions.js
+++ functions.js
@@ -1,3 +1,9 @@
+/**
+ * Check variable if undefined, null or empty.
+ *
+ * @param variable Variable
+ * @returns {boolean}
+ */
 const notDefined = (variable) => {
 	return typeof variable === 'undefined' || variable === null || !variable;
 };
