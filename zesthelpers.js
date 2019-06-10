ccccc6a07e216bbf839d18c6b7cb6cafd430441a
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-06-10 13:20:53 +0800

    Added module exports for functions and ZestApi helpers.

--- zesthelpers.js
+++ zesthelpers.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const { notDefined } = require('./functions');
+const { ZestApi } = require('./ZestApi');
+
+const helloWorld = () => {
+	console.log('Hello World');
+};
+
+module.exports.notDefined = notDefined;
+module.exports.ZestApi = ZestApi;
+module.exports.helloWorld = helloWorld;
