c1fc6c40ed583b19f5dce68021f1bb24ef27dcbc
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-06-12 12:32:31 +0800

    Fixed the exporting of page classes.

--- zesthelpers.js
+++ zesthelpers.js
@@ -1,25 +1,25 @@
 const { notDefined } = require('./helpers/functions');
 const { ZestApi } = require('./helpers/ZestApi');
-const { Zest4CategoryPage } = require('./pages/zest4/CategoryPage');
-const { Zest4CheckoutPage } = require('./pages/zest4/CheckoutPage');
-const { Zest4ContactUs } = require('./pages/zest4/ContactUs');
-const { Zest4EnquiryReceiptPage } = require('./pages/zest4/EnquiryReceiptPage');
-const { Zest4HomePage } = require('./pages/zest4/HomePage');
-const { Zest4Page } = require('./pages/zest4/Page');
-const { Zest4ProductPage } = require('./pages/zest4/ProductPage');
-const { Zest4ReceiptPage } = require('./pages/zest4/ReceiptPage');
+
+// Zest 4 Pages
+const Zest4CategoryPage = require('./pages/zest4/CategoryPage').CategoryPage;
+const Zest4CheckoutPage = require('./pages/zest4/CheckoutPage').CheckoutPage;
+const Zest4ContactUs = require('./pages/zest4/ContactUs').ContactUs;
+const Zest4EnquiryReceiptPage = require('./pages/zest4/EnquiryReceiptPage').EnquiryReceiptPage;
+const Zest4HomePage = require('./pages/zest4/HomePage').HomePage;
+const Zest4Page = require('./pages/zest4/Page').Page;
+const Zest4ProductPage = require('./pages/zest4/ProductPage').ProductPage;
+const Zest4ReceiptPage = require('./pages/zest4/ReceiptPage').ReceiptPage;
 
 // export helper modules
 module.exports.notDefined = notDefined;
 module.exports.ZestApi = ZestApi;
 // export zest4 pages class
-module.exports = {
-	Zest4CategoryPage,
-	Zest4CheckoutPage,
-	Zest4ContactUs,
-	Zest4EnquiryReceiptPage,
-	Zest4HomePage,
-	Zest4ProductPage,
-	Zest4ReceiptPage,
-	Zest4Page
-};
+module.exports.Zest4CategoryPage = Zest4CategoryPage;
+module.exports.Zest4CheckoutPage = Zest4CheckoutPage;
+module.exports.Zest4ContactUs = Zest4ContactUs;
+module.exports.Zest4EnquiryReceiptPage = Zest4EnquiryReceiptPage;
+module.exports.Zest4HomePage = Zest4HomePage;
+module.exports.Zest4ProductPage = Zest4ProductPage;
+module.exports.Zest4ReceiptPage = Zest4ReceiptPage;
+module.exports.Zest4Page = Zest4Page;
