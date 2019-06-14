7c6296e7b77e867fa419bd1ba337af212a0d278d
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-06-14 09:10:14 +0800

    Added Zest3 Category page.

---
+++ pages/zest3/CategoryPage.js
@@ -0,0 +1,86 @@
+const { By, until } = require('selenium-webdriver');
+const { Page } = require('./Page');
+
+/**
+ * Category page class
+ *
+ */
+class CategoryPage extends Page {
+	/**
+	 * Constructor
+	 *
+	 * @param webdriver Selenium web driver.
+	 * @param url The url of the page.
+	 * @param catViewLocator The category view locator
+	 * @param productLinkLocator The product view locator
+	 */
+	constructor(webdriver, url, catViewLocator, productLinkLocator) {
+		super(webdriver, url);
+
+		this._catViewLocator = catViewLocator;
+		this._productLinkLocator = productLinkLocator;
+
+		// initialize locators if not defined
+		this._catViewLocator = notDefined(this._catViewLocator) ?
+			By.css('div.catview') : this._catViewLocator;
+		this._productLinkLocator = notDefined(this._productLinkLocator) ?
+			By.css('.item-list .item-cell a') : this._productLinkLocator;
+	}
+
+	/**
+	 * Set category view locator
+	 *
+	 * @param value Locator2
+	 */
+	set catViewLocator(value) {
+		this._catViewLocator = value;
+	}
+
+	/**
+	 * Set product link locator
+	 *
+	 * @param value Locator
+	 */
+	set productLinkLocator(value) {
+		this._productLinkLocator = value;
+	}
+
+	/**
+	 * Get the product url
+	 *
+	 * @returns {Promise<any[]|*>}
+	 */
+	async getProductURLs() {
+		// locate the cat view
+		const catView = await this._driver.findElement(this._catViewLocator);
+		// wait a bit until it is visible
+		await this._driver.wait(until.elementIsVisible(catView), 2000);
+		// get product links
+		let productURLs = await catView.findElements(this._productLinkLocator);
+
+		if (productURLs.length === 0) {
+			// use the zest 3 selectors
+			productURLs = await catView.findElements(By.css('.product-collection .product-card a'));
+		}
+
+		this._productURLs = await this.resolveURLs(productURLs).then((urls) => urls);
+		return this._productURLs;
+	}
+
+	/**
+	 * Resolve url promises to extract href attribute
+	 *
+	 * @param urlPromises URL element promises
+	 * @return {Promise<any[]>}
+	 */
+	async resolveURLs(urlPromises) {
+		const promises = [];
+		urlPromises.forEach((element) => {
+			promises.push(element.getAttribute('href'));
+		});
+
+		return Promise.all(promises);
+	}
+}
+
+module.exports.CategoryPage = CategoryPage;
