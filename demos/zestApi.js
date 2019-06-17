180c7989dd6ba04a776e2e9f3018b1a50f5ffe5e
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-06-17 09:12:33 +0800

    Added demo for transaction method.

--- demos/zestApi.js
+++ demos/zestApi.js
@@ -4,8 +4,13 @@ const { ZestApi } = require('../helpers/ZestApi');
 dotenv.config();
 
 const zestApi = new ZestApi(process.env.API_BASE_URL, process.env.API_KEY);
-
+/*
 zestApi.orderLines('ORD0001648').then((orders) => {
+	console.log(orders);
+});
+ */
+
+zestApi.transaction('ORD0001646').then((orders) => {
 	console.log(orders);
 });
 
