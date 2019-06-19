44202df1c9b5a0a28d1448f71a838e85753023fc
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-06-19 10:02:58 +0800

    Added pickRandom function that can return random item from list or array.

--- helpers/functions.js
+++ helpers/functions.js
@@ -8,5 +8,16 @@ const notDefined = (variable) => {
 	return typeof variable === 'undefined' || variable === null || !variable;
 };
 
+/**
+ * Pick a random element from the list.
+ *
+ * @param items Array
+ * @returns {*}
+ */
+const pickRandom = (...items) => {
+	return items[Math.floor(Math.random()*items.length)];
+};
+
 module.exports.notDefined = notDefined;
+module.exports.pickRandom = pickRandom;
 
