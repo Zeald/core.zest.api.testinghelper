468c597cd516cc42d7f62eafba05cb435dcc5f34
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-06-24 13:16:25 +0800

    Created the driver builder class for chrome.

--- helpers/ChromeDriverBuilder.js
+++ helpers/ChromeDriverBuilder.js
@@ -1,3 +1,53 @@
 const { Builder } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 const path = require('path');
 const dotenv = require('dotenv');
+
+dotenv.config();
+
+// this empty require is needed because windows 10 having PATH issues.
+require('chromedriver');
+
+/**
+ * ChromeDriverBuilder class.
+ *
+ */
+class ChromeDriverBuilder {
+	/**
+	 * Constructor
+	 *
+	 * @param envPath Path of the env file
+	 * @param screen Screen config
+	 */
+	constructor(envPath, screen) {
+		if (envPath) {
+			dotenv.config({
+				path: path.resolve(__dirname+envPath)
+			});
+		}
+
+		this._screen = screen;
+	}
+
+	/**
+	 * Build method
+	 *
+	 * @returns {ThenableWebDriver}
+	 */
+	build() {
+		let driver = null;
+
+		if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
+			driver = new Builder()
+				.forBrowser('chrome')
+				.setChromeOptions(new chrome.Options().headless().windowSize(this._screen))
+				.build();
+		} else {
+			driver = new Builder().forBrowser('chrome').build();
+		}
+
+		return driver;
+	}
+}
+
+module.exports.ChromeDriverBuilder = ChromeDriverBuilder;
