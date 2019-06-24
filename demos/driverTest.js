c86f6a10897d5aaf0bb40c03aad9619be2bf28c1
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-06-24 15:36:59 +0800

    Added testing of searching a term.

--- demos/driverTest.js
+++ demos/driverTest.js
@@ -1,4 +1,5 @@
 const { DriverFactory } = require('../helpers/DriverFactory');
+const { HomePage } = require('../pages/zest4/HomePage');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -13,6 +14,17 @@ const driver = DriverFactory.build({
 describe('Test', () => {
 	it('Driver', async () => {
 		await driver.get(process.env.TEST_BASE_URL);
+	});
+
+	it('Home page search', async () => {
+		const homePage = new HomePage(
+			driver,
+			process.env.TEST_BASE_URL
+		);
+
+		await homePage.open();
+		await homePage.closeModalIfExist();
+		await homePage.searchTerm('ambot');
 	});
 
 	after(async () => {
