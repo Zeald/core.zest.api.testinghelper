a24fe5a0b8c919c8db545755a44faa56fbe85dbc
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-06-28 11:55:24 +0800

    Added demo test scripts.

--- demos/zestApi.js
+++ demos/zestApi.js
@@ -3,14 +3,23 @@ const { ZestApi } = require('../helpers/ZestApi');
 
 dotenv.config();
 
-const zestApi = new ZestApi(process.env.API_BASE_URL, process.env.API_KEY);
+const zestApi = new ZestApi({
+	apiBaseURL: process.env.API_BASE_URL,
+	apiKey: process.env.API_KEY,
+	apiEmailLogBaseURL: process.env.API_EMAIL_LOG_URL
+});
 /*
 zestApi.orderLines('ORD0001648').then((orders) => {
 	console.log(orders);
 });
- */
+
 
 zestApi.transaction('ORD0001646').then((orders) => {
 	console.log(orders);
+});
+ */
+
+zestApi.checkOrderAcknowledgmentEmail('ORD0001936', 'email.log', 20).then((data) => {
+	console.log(data);
 });
 
